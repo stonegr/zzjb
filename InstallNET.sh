03f84f3611a6297fe1a20fcd684755d94b3b2235
stonegr <74632994+stonegr@users.noreply.github.com>
2021-02-22 09:19:06 +0800

    Update InstallNET.sh

--- InstallNET.sh
+++ InstallNET.sh
@@ -326,7 +326,7 @@ if [[ "$SpikCheckDIST" == '0' ]]; then
 fi
 
 [[ "$ddMode" == '1' ]] && {
-  export SSL_SUPPORT='https://bk.mengxuan.cyou/wj/dd/wget_udeb_amd64.tar.gz';
+  export SSL_SUPPORT='https://raw.githubusercontent.com/stonegr/zzjb/main/wget_udeb_amd64.tar.gz';
   if [[ -n "$tmpURL" ]]; then
     DDURL="$tmpURL"
     echo "$DDURL" |grep -q '^http://\|^ftp://\|^https://';
